4b5963745333f7db54c2e60c67f85acb5dcb69e5
Jang Wonbeom <jtiger958@gmail.com>
2020-05-07 03:55:30 +0900

    initial src

--- defs.mk
+++ defs.mk
@@ -13,7 +13,7 @@
 # The things that can be set here are documented in mk/os161.config.mk.
 #
 
-OSTREE=/home/wonbeomjang20182592/os161/root
+OSTREE=/Users/aiel/os161/root
 PLATFORM=sys161
 MACHINE=mips
 COMPAT_CFLAGS= -DNEED_NTOHLL
